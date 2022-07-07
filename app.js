3d38b15dea16c61e3ee02200292eb58040cba004
abhinavS1911 <95422109+abhinavS1911@users.noreply.github.com>
2022-07-07 23:36:39 +0530

    Use HTTP again instead of Fetch

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const http = require("http");
 const { json } = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/prototype.html");
 });
 
 app.post("/", function (req, res) {});
 
 app.post("/posts", function (req, res) {
-  // completion handler
   const postUrl = "http://jsonplaceholder.typicode.com/posts";
   http.get(postUrl, function (response) {
     // on is a method to tap into server's data and search through it
     response.on("data", function (data) {
       const postData = JSON.parse(data);
-      console.log(postData);
     });
   });
 });
@@ -31,10 +28,9 @@ app.post("/albums", function (req, res) {
     // on is a method to tap into server's data and search through it
     response.on("data", function (data) {
       const postData = JSON.parse(data);
-      console.log(postData);
     });
+  });
 });
-
 app.listen(3000, function (req, res) {
   console.log("Server is running on port 3000.");
 });
